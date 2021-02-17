b26d71c0f74da92d60d4be61b9ce3dd00e9917a2
Lev Mayorov <jiebbi4@gmail.com>
2021-02-18 02:21:39 +0300

    fix

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 8080;
-const host = process.env.HOST || 'localhost';
 
 app.use(express.static(__dirname + '/dist/v-chat-angular'));
 
@@ -12,9 +11,6 @@ app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/v-chat-angular/index.html'));
 });
 
-const server = app.listen(port, host, () => {
-  const host = server.address().address;
-  const port = server.address().port;
-
-  console.log('App listening at http://%s:%s', host, port)
+app.listen(port, () => {
+  console.log('Server started', port)
 })
